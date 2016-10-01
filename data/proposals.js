94a7fa4de2904b29367a77a5284242f49906b062
Michał Budzyński <michal@virtualdesign.pl>
2016-10-01 03:38:00 +0100

    :raising_hand: move data to the new place

---
+++ data/proposals.js
@@ -0,0 +1,519 @@
+module.exports = [
+  {
+    "title": "Agent Carter",
+    "imdb": "tt3475734",
+    "episodes": 0,
+    "stillWatching": 1,
+    "votes": [
+      "michalbe"
+    ]
+  },
+  {
+    "title": "Almost Human",
+    "imdb": "tt2654580",
+    "stillWatching": 1,
+    "votes": [
+      "scryptonite"
+    ]
+  },
+  {
+    "title": "Angie Tribeca",
+    "imdb": "tt3597790",
+    "stillWatching": 0,
+    "votes": [
+      "siemiatj"
+    ]
+  },
+  {
+    "title": "Avatar: The Last Airbender",
+    "imdb": "tt0417299",
+    "stillWatching": 0,
+    "votes": [
+      "guamaso",
+      "alexpnt"
+    ],
+    "wiki": "Avatar:_The_Last_Airbender"
+  },
+  {
+    "title": "Band of Brothers",
+    "imdb": "tt0185906",
+    "stillWatching": 0,
+    "votes": [
+      "tobytailor",
+      "stasm",
+      "kasprownik",
+      "siemiatj"
+    ]
+  },
+  {
+    "title": "Battlestar Galactica (2004_TV_series)",
+    "imdb": "tt0407362",
+    "stillWatching": 0,
+    "votes": [
+      "stasm",
+      "doogi",
+      "matisnape"
+    ]
+  },
+  {
+    "title": "Battlestar Galactica (TV miniseries)",
+    "imdb": "tt0314979",
+    "stillWatching": 0,
+    "votes": [
+      "stasm",
+      "doogi"
+    ]
+  },
+  {
+    "title": "Better Call Saul",
+    "imdb": "tt3032476",
+    "episodes": 0,
+    "stillWatching": 1,
+    "votes": [
+      "michalbe"
+    ]
+  },
+  {
+    "title": "Broadchurch",
+    "imdb": "tt2249364",
+    "stillWatching": 0,
+    "votes": [
+      "matisnape"
+    ]
+  },
+  {
+    "title": "Bron/Broen",
+    "imdb": "tt1733785",
+    "stillWatching": 0,
+    "votes": [
+      "kasprownik"
+    ],
+    "wiki": "The Bridge (Danish/Swedish TV series)"
+  },
+  {
+    "title": "Burn Notice",
+    "imdb": "tt0810788",
+    "stillWatching": 0,
+    "votes": [
+      "1marc"
+    ]
+  },
+  {
+    "title": "Chuck",
+    "imdb": "tt0934814",
+    "stillWatching": 1,
+    "votes": [
+      "kamilogorek",
+      "gstepniewski",
+      "scryptonite",
+      "tiagomachado"
+    ]
+  },
+  {
+    "title": "Childrens Hospital",
+    "imdb": "tt1325113",
+    "stillWatching": 0,
+    "votes": [
+      "siemiatj"
+    ]
+  },
+  {
+    "title": "Community",
+    "imdb": "tt1439629",
+    "stillWatching": 1,
+    "votes": [
+      "gstepniewski"
+    ]
+  },
+  {
+    "title": "Continuum",
+    "imdb": "tt1954347",
+    "stillWatching": 1,
+    "votes": [
+      "scryptonite"
+    ]
+  },
+  {
+    "title": "Cosmos",
+    "imdb": "tt0081846",
+    "stillWatching": 0,
+    "votes": [
+      "alexpnt"
+    ]
+  },
+  {
+    "title": "Cowboy Bebop",
+    "imdb": "tt0213338",
+    "stillWatching": 0,
+    "votes": [
+      "datrio",
+      "kamilogorek",
+      "stasm",
+      "stasm",
+      "guamaso",
+      "siemiatj"
+    ]
+  },
+  {
+    "title": "Deadwood",
+    "imdb": "tt0348914",
+    "episodes": 0,
+    "stillWatching": 1,
+    "votes": [
+      "michalbe"
+    ]
+  },
+  {
+    "title": "Dollhouse",
+    "imdb": "tt1135300",
+    "stillWatching": 0,
+    "votes": [
+      "igorw",
+      "guamaso",
+      "doogi"
+    ]
+  },
+  {
+    "title": "Firefly",
+    "imdb": "tt0303461",
+    "stillWatching": 0,
+    "votes": [
+      "stasm",
+      "guamaso",
+      "alexpnt",
+      "doogi"
+    ]
+  },
+  {
+    "title": "High Profits",
+    "imdb": "tt4468340",
+    "episodes": 8,
+    "stillWatching": 0,
+    "votes": [
+      "egweeks"
+    ]
+  },
+  {
+    "title": "It's Always Sunny in Philadelphia",
+    "imdb": "tt0472954",
+    "stillWatching": 1,
+    "votes": [
+      "gstepniewski",
+      "ryanburgess",
+      "stkrzysiak"
+    ]
+  },
+  {
+    "title": "Jessica Jones",
+    "imdb": "tt2357547",
+    "stillWatching": 1,
+    "votes": ["ryanburgess"]
+  },
+  {
+    "title": "Master of None",
+    "imdb": "tt4635276",
+    "stillWatching": 1,
+    "votes": ["ryanburgess"]
+  },
+  {
+    "title": "My Name Is Earl",
+    "imdb": "tt0460091",
+    "stillWatching": 0,
+    "votes": [
+      "kamilogorek",
+      "gstepniewski",
+      "stkrzysiak"
+    ]
+  },
+  // {
+  //   "title": "Narcos",
+  //   "imdb": "tt2707408",
+  //   "stillWatching": 1,
+  //   "votes": ["ryanburgess"]
+  // },
+  {
+    "title": "Numb3rs",
+    "imdb": "tt0433309",
+    "stillWatching": 0,
+    "votes": [
+      "afronski"
+    ],
+    "wiki": "Numbers (TV series)"
+  },
+  {
+    "title": "Oz",
+    "imdb": "tt0118421",
+    "stillWatching": 0,
+    "votes": [
+      "kamilogorek",
+      "stkrzysiak",
+      "egweeks"
+    ]
+  },
+  // {
+  //   "title": "Parks and Recreation",
+  //   "imdb": "tt1266020",
+  //   "stillWatching": 1,
+  //   "votes": [
+  //     "kamilogorek",
+  //     "gstepniewski",
+  //     "ryanburgess",
+  //     "egweeks"
+  //   ]
+  // },
+  {
+    "title": "Person of Interest",
+    "imdb": "tt1839578",
+    "stillWatching": 0,
+    "votes": [
+      "shorang",
+      "scryptonite",
+      "egweeks"
+    ],
+    "wiki": "Person of Interest (TV series)"
+  },
+  {
+    "title": "Ray Donovan",
+    "imdb": "tt2249007",
+    "stillWatching": 1,
+    "votes": [
+      "ziolek666"
+    ]
+  },
+  // {
+  //   "title": "Robot Chicken",
+  //   "imdb": "tt0437745",
+  //   "stillWatching": 1,
+  //   "votes": [
+  //     "stasm"
+  //   ]
+  // },
+  {
+    "title": "Smallville",
+    "imdb": "tt0279600",
+    "stillWatching": 0,
+    "votes": [
+      "alexpnt"
+    ]
+  },
+  {
+    "title": "Sons of Anarchy",
+    "imdb": "tt1124373",
+    "stillWatching": 1,
+    "votes": [
+      "kamilogorek",
+      "tobytailor",
+      "tizianozonta",
+      "ryanburgess",
+      "stkrzysiak"
+    ]
+  },
+  {
+    "title": "Spartacus",
+    "imdb": "tt1442449",
+    "stillWatching": 1,
+    "votes": [
+      "kamilogorek",
+      "datrio",
+      "afronski",
+      "tobytailor"
+    ]
+  },
+  {
+    "title": "Supernatural",
+    "imdb": "tt0460681",
+    "stillWatching": 0,
+    "votes": [
+      "matisnape",
+      "tiagomachado"
+    ]
+  },
+  {
+    "title": "The Elegant Universe",
+    "imdb": "tt0377171",
+    "stillWatching": 0,
+    "votes": [
+      "alexpnt"
+    ]
+  },
+  {
+    "title": "The Following",
+    "imdb": "tt2071645",
+    "stillWatching": 0,
+    "votes": [
+      "shorang",
+      "ryanburgess"
+    ]
+  },
+  {
+    "title": "The Lost Room",
+    "imdb": "tt0830361",
+    "stillWatching": 0,
+    "votes": [
+      "matisnape"
+    ]
+  },
+  {
+    "title": "The Newsroom",
+    "imdb": "tt1870479",
+    "stillWatching": 1,
+    "votes": [
+      "tobytailor",
+      "kasprownik"
+    ],
+    "wiki": "The Newsroom (U.S. TV series)"
+  },
+  {
+    "title": "The Office",
+    "imdb": "tt0386676",
+    "stillWatching": 0,
+    "votes": [
+      "kamilogorek",
+      "ryanburgess",
+      "tiagomachado",
+      "egweeks"
+    ],
+    "wiki": "The Office (U.S. TV series)"
+  },
+  {
+    "title": "The Pacific",
+    "imdb": "tt0374463",
+    "stillWatching": 0,
+    "votes": [
+      "tobytailor"
+    ],
+    "wiki": "The_Pacific_(TV_miniseries)"
+  },
+  {
+    "title": "The Shield",
+    "imdb": "tt0286486",
+    "stillWatching": 0,
+    "votes": [
+      "tobytailor"
+    ]
+  },
+  {
+    "title": "The Strain",
+    "imdb": "tt2654620",
+    "stillWatching": 0,
+    "votes": [
+      "chemikpil",
+      "kasprownik"
+    ]
+  },
+  {
+    "title": "The Walking Dead",
+    "imdb": "tt1520211",
+    "stillWatching": 1,
+    "votes": [
+      "datrio",
+      "afronski",
+      "tobytailor",
+      "shorang",
+      "ryanburgess",
+      "scryptonite",
+      "chemikpil",
+      "alexpnt",
+      "doogi",
+      "tiagomachado",
+      "egweeks"
+    ],
+    "wiki": "The Walking Dead (TV series)"
+  },
+  {
+    "title": "The West Wing",
+    "imdb": "tt0200276",
+    "stillWatching": 0,
+    "votes": [
+      "datrio"
+    ]
+  },
+  {
+    "title": "Touch",
+    "imdb": "tt1821681",
+    "stillWatching": 0,
+    "votes": [
+      "alexpnt"
+    ]
+  },
+  {
+    "title": "Transformers: Prime",
+    "imdb": "tt1659175",
+    "stillWatching": 0,
+    "votes": [
+      "guamaso"
+    ],
+    "wiki": "Transformers:_Prime"
+  },
+  {
+    "title": "Tron: Uprising",
+    "imdb": "tt1812523",
+    "stillWatching": 0,
+    "votes": [
+      "guamaso"
+    ],
+    "wiki": "Tron:_Uprising"
+  },
+  {
+    "title": "Twin Peaks",
+    "imdb": "tt0098936",
+    "stillWatching": 0,
+    "votes": [
+      "zigomir"
+    ]
+  },
+  // {
+  //   "title": "Unbreakable Kimmy Schmidt",
+  //   "imdb": "tt3339966",
+  //   "stillWatching": 1,
+  //   "votes": ["ryanburgess"]
+  // },
+  {
+    "title": "Utopia",
+    "imdb": "tt2384811",
+    "stillWatching": 0,
+    "votes": [
+      "matisnape",
+      "kasprownik"
+    ]
+  },
+  {
+    "title": "Vikings",
+    "imdb": "tt2306299",
+    "episodes": 0,
+    "stillWatching": 1,
+    "votes": [
+      "michalbe"
+    ]
+  },
+  {
+    "title": "Warehouse 13",
+    "imdb": "tt1132290",
+    "stillWatching": 0,
+    "votes": [
+      "hounddog"
+    ]
+  },
+  {
+    "title": "Weeds",
+    "imdb": "tt0439100",
+    "stillWatching": 0,
+    "votes": [
+      "tizianozonta",
+      "ryanburgess",
+      "mmalecki",
+      "stkrzysiak"
+    ],
+    "wiki": "Weeds_(TV_series)"
+  },
+  {
+    "title": "Wilfred",
+    "imdb": "tt1703925",
+    "stillWatching": 1,
+    "votes": [
+      "kamilogorek",
+      "gstepniewski"
+    ],
+    "wiki": "Wilfred (U.S. TV series)"
+  }
+];
